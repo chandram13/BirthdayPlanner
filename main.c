c8e93c49e2552a22c815f3fd1a20c147f239a94f
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-03 10:12:46 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+// Marvish Chandra
+
+#include <stdio.h>
+
+int main birthdayPlan(){
+    budget = int;
+    themes = ["Carnival","80s Bash","Circus","Sports","Princess Celebration","Dog Themed","Rainbow Celebration","Camping","Mermaid","Beach Bash","Dinosaur Jam","Garden Gala","Karaoke Party","Artsy","Christmas","Talent Show","Hip Hop","Pop","Country","Reggaeton"];
+    for (i = 0; i < 15; i++){
+        printf(themes[i]);
+    };
+    guestNumber = int;
+    venues = ["At Home","Restaurant","Party Hall","Banquet Hall"]
+    plannedActivities = ["Socializing","Games","Birthday Song","Cutting of the Cake","Gift Giving"]
+};
